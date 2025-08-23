a93f307a14ce853f355b4bd8cc316cabcb8e1c9b
JayVillain <saipi3825@gmail.com>
2025-08-23 09:25:55 +0000

    Aug 23, 2025, 4:25 PM

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_number', 'user_id', 'table_id', 'type', 'total_price', 'status'];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
