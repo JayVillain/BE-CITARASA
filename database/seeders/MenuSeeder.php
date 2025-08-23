84bbfa027cba483fb2cb3c541b7831befdc2b44e
JayVillain <saipi3825@gmail.com>
2025-08-23 13:33:01 +0000

    Aug 23, 2025, 8:33 PM

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MenuSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('menus')->insert([
+            [
+                'name' => 'Nasi Goreng Spesial',
+                'description' => 'Nasi goreng dengan telur, ayam, dan sayuran segar',
+                'price' => 25000,
+                'menu_category_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Ayam Bakar Madu',
+                'description' => 'Ayam bakar dengan bumbu madu dan rempah pilihan',
+                'price' => 35000,
+                'menu_category_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Gado-Gado Jakarta',
+                'description' => 'Sayuran segar dengan bumbu kacang khas Jakarta',
+                'price' => 20000,
+                'menu_category_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
