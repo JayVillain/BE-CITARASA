ab88c1d15e75a303bf98cb9e10d1082076c92853
JayVillain <saipi3825@gmail.com>
2025-08-23 13:54:06 +0000

    Aug 23, 2025, 8:54 PM

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -3,37 +3,32 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\Menu;
 
 class MenuSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('menus')->insert([
-            [
-                'name' => 'Nasi Goreng Spesial',
-                'description' => 'Nasi goreng dengan telur, ayam, dan sayuran segar',
-                'price' => 25000,
-                'menu_category_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Ayam Bakar Madu',
-                'description' => 'Ayam bakar dengan bumbu madu dan rempah pilihan',
-                'price' => 35000,
-                'menu_category_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Gado-Gado Jakarta',
-                'description' => 'Sayuran segar dengan bumbu kacang khas Jakarta',
-                'price' => 20000,
-                'menu_category_id' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+        Menu::create([
+            'name' => 'Nasi Goreng Spesial',
+            'description' => 'Nasi goreng dengan telur, ayam, dan sayuran segar',
+            'price' => 25000,
+            'menu_category_id' => 1,
+            'tag' => 'Populer'
+        ]);
+
+        Menu::create([
+            'name' => 'Ayam Bakar Madu',
+            'description' => 'Ayam bakar bumbu madu dan rempah',
+            'price' => 35000,
+            'menu_category_id' => 1,
+        ]);
+
+        Menu::create([
+            'name' => 'Es Teh Manis',
+            'description' => 'Minuman segar teh manis dingin',
+            'price' => 5000,
+            'menu_category_id' => 2,
         ]);
     }
 }
