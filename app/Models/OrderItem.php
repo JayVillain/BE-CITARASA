52d38c92eeaab6deb647e64cfdc93260aac37fd1
JayVillain <saipi3825@gmail.com>
2025-08-23 09:26:03 +0000

    Aug 23, 2025, 4:26 PM

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'menu_id', 'quantity', 'price', 'subtotal'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
 }
