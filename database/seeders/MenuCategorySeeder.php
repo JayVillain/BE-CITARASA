9ba9f0a40c5f3a270f6964879904bb7729e0090c
JayVillain <saipi3825@gmail.com>
2025-08-23 13:35:40 +0000

    Aug 23, 2025, 8:35 PM

--- database/seeders/MenuCategorySeeder.php
+++ database/seeders/MenuCategorySeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MenuCategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('menu_categories')->insert([
+            ['name' => 'Makanan', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Minuman', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Dessert', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Snack', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
