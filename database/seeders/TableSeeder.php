e3666b2336cf01e959a02067910a6ff9f6edbd0d
JayVillain <saipi3825@gmail.com>
2025-08-23 13:52:26 +0000

    Aug 23, 2025, 8:52 PM

--- database/seeders/TableSeeder.php
+++ database/seeders/TableSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Table;
 
 class TableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Table::create(['number' => '01', 'capacity' => 4, 'status' => 'available']);
+        Table::create(['number' => '02', 'capacity' => 2, 'status' => 'available']);
+        Table::create(['number' => '03', 'capacity' => 6, 'status' => 'available']);
     }
 }
