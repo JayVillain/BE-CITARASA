d28bfae430f119d58eb6ffd70026bc649edb9c87
JayVillain <saipi3825@gmail.com>
2025-08-23 09:29:14 +0000

    Aug 23, 2025, 4:29 PM

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
-<?php
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\MenuController;
+use App\Http\Controllers\API\TableController;
+use App\Http\Controllers\API\OrderController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::apiResource('menus', MenuController::class);
+    Route::apiResource('tables', TableController::class);
+    Route::apiResource('orders', OrderController::class);
+
+    Route::get('/orders/active', [OrderController::class, 'active']);
+    Route::get('/orders/history', [OrderController::class, 'history']);
+    Route::get('/dashboard', [OrderController::class, 'dashboard']);
 });
