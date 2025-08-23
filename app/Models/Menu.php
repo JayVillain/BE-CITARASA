53bfec0bee090456ad1f1a28f149af860e3170c5
JayVillain <saipi3825@gmail.com>
2025-08-23 09:25:21 +0000

    Aug 23, 2025, 4:25 PM

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Menu extends Model
+class MenuCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function menus()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
