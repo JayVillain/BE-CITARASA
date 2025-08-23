33c7a6f12e1c518c44de7eec94ddeac7b01d50af
JayVillain <saipi3825@gmail.com>
2025-08-23 09:25:34 +0000

    Aug 23, 2025, 4:25 PM

--- app/Models/MenuCategory.php
+++ app/Models/MenuCategory.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MenuCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function menus()
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
