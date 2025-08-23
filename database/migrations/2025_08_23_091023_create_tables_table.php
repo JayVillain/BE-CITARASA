5998e5f678afd9e1bad9de85ef9802fd42fdcf77
JayVillain <saipi3825@gmail.com>
2025-08-23 13:46:03 +0000

    Aug 23, 2025, 8:46 PM

--- database/migrations/2025_08_23_091023_create_tables_table.php
+++ database/migrations/2025_08_23_091023_create_tables_table.php
@@ -10,8 +10,10 @@ return new class extends Migration
     {
         Schema::create('tables', function (Blueprint $table) {
             $table->id();
-            $table->string('table_number')->unique();
+            $table->string('number')->unique(); // Nomor meja
             $table->enum('status', ['available', 'occupied', 'reserved', 'cleaning'])->default('available');
+            $table->integer('capacity')->default(4); // kapasitas kursi
+            $table->string('qr_code')->nullable();   // bisa simpan kode unik
             $table->timestamps();
         });
     }
